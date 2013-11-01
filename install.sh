06396132048e6915ab81bd390bc6c40c969b6498
Ryan Lucchese <ryan.lucchese@perceptivesoftware.com>
2013-11-01 09:33:51 -0600

    source ~/.profile after creating ~/bin, added some comments

--- install.sh
+++ install.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+# if $HOME/bin doesn't exist, create it
 if [ ! -d $HOME/bin ]; then
 	mkdir $HOME/bin
+	# in many cases the default ~/.profile will add $HOME/bin to path if it exists
+	if [ -e $HOME/.profile ]; then
+		. $HOME/.profile
+	fi
 fi
 
+# make sure $HOME/bin is in $PATH, otherwise this install script will not work
 if [[ ":$PATH:" != *":$HOME/bin:"* ]]; then
 	echo "$HOME/bin is not in \$PATH! Please set \$PATH appropriately"
 	exit 1
